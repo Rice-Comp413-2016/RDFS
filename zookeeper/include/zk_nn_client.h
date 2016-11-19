d19628fd436f0a9d77dd3bc88cb01deda78ca9ab
Kyler Mejia <mejia.kyler@gmail.com>
2016-11-19 21:46:01 +0000

    watchers for datanode death

--- zookeeper/include/zk_nn_client.h
+++ zookeeper/include/zk_nn_client.h
@@ -186,6 +186,17 @@ class ZkNnClient : public ZkClientCommon {
 		bool buildExtendedBlockProto(ExtendedBlockProto* eb, const std::uint64_t& block_id,
 											const uint64_t& block_size);
 
+
+		/**
+		 * Watches /health for new datanodes, attaches watchers to new datanodes' heartbeats.
+		 */
+		static void watcher_health(zhandle_t *zzh, int type, int state, const char *path, void *watcherCtx);
+		
+		/**
+		 * Watches datanode heartbeats.
+		 */
+		static void watcher_health_child(zhandle_t *zzh, int type, int state, const char *path, void *watcherCtx);
+
 		/**
 		 * Returns the current timestamp in milliseconds
 		 */
